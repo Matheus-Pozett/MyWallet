772da8cb758935fcc990f586165b5902a4cc5570
Matheus Pozett <maa_souza11@hotmail.com>
2025-05-05 09:42:53 -0300

    feat: cria componente header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,23 @@ import { useSelector } from 'react-redux';
 import { TGlobalState } from '../types';
 
 function Header() {
-  const user = useSelector((globalState: TGlobalState) => globalState.user);
-  return <div>{user.email}</div>;
+  const { email } = useSelector((globalState: TGlobalState) => globalState.user);
+
+  return (
+    <header>
+      <img
+        src="/mywallet.svg"
+        alt="logo"
+      />
+      <p
+        data-testid="total-field"
+      >
+        {`Total de despesas: ${0} `}
+        <span data-testid="header-currency-field">BRL</span>
+      </p>
+      <p data-testid="email-field">{email}</p>
+    </header>
+  );
 }
 
 export default Header;
