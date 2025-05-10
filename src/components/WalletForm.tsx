71e02ab6876387bd8af2a61785303d8eee81e93e
Matheus Pozett <maa_souza11@hotmail.com>
2025-05-10 13:23:58 -0300

    style: css do componente walletform

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { addExpenseThunk, editExpense, getCurrenciesApi } from '../redux/actions';
 import { AppDispatch, TGlobalState } from '../types';
+import './formWallet.css';
 
 type TWalletForm = {
   description: string,
@@ -53,61 +54,73 @@ function WalletForm() {
   };
 
   return (
-    <form onSubmit={ handleSubmit(onSubmit) }>
-      <label htmlFor="despesa">Descrição da despesa</label>
-      <input
-        type="text"
-        id="despesa"
-        data-testid="description-input"
-        { ...register('description') }
-      />
+    <div className="wallet-form">
+      <form className="form-container" onSubmit={ handleSubmit(onSubmit) }>
+        <div className="desc-tag-container">
+          <label htmlFor="despesa">Descrição da despesa</label>
+          <input
+            type="text"
+            id="despesa"
+            className="description-input"
+            data-testid="description-input"
+            { ...register('description') }
+          />
+          <label htmlFor="tag">Categoria da despesa</label>
+          <select
+            data-testid="tag-input"
+            className="tag-input"
+            id="tag"
+            { ...register('tag') }
+          >
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </div>
 
-      <label htmlFor="tag">Categoria da despesa</label>
-      <select
-        data-testid="tag-input"
-        id="tag"
-        { ...register('tag') }
-      >
-        <option value="Alimentação">Alimentação</option>
-        <option value="Lazer">Lazer</option>
-        <option value="Trabalho">Trabalho</option>
-        <option value="Transporte">Transporte</option>
-        <option value="Saúde">Saúde</option>
-      </select>
+        <div className="value-method-container">
+          <label htmlFor="valor">Valor</label>
+          <input
+            type="number"
+            id="valor"
+            className="value-input"
+            data-testid="value-input"
+            { ...register('value') }
+          />
 
-      <label htmlFor="valor">Valor</label>
-      <input
-        type="number"
-        id="valor"
-        data-testid="value-input"
-        { ...register('value') }
-      />
+          <label htmlFor="metodo">Método de pagamento</label>
+          <select
+            data-testid="method-input"
+            className="method-input"
+            id="metodo"
+            { ...register('method') }
+          >
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
 
-      <label htmlFor="metodo">Método de pagamento</label>
-      <select
-        data-testid="method-input"
-        id="metodo"
-        { ...register('method') }
-      >
-        <option value="Dinheiro">Dinheiro</option>
-        <option value="Cartão de crédito">Cartão de crédito</option>
-        <option value="Cartão de débito">Cartão de débito</option>
-      </select>
-
-      <label htmlFor="moeda">Moeda</label>
-      <select
-        data-testid="currency-input"
-        id="moeda"
-        { ...register('currency') }
-      >
-        {currencies.map((data) => (
-          <option key={ data } value={ data }>{data}</option>
-        ))}
-      </select>
-      <button type="submit">
-        {editor ? 'Editar despesa' : 'Adicionar despesa'}
-      </button>
-    </form>
+          <label htmlFor="moeda">Moeda</label>
+          <select
+            data-testid="currency-input"
+            className="currency-input"
+            id="moeda"
+            { ...register('currency') }
+          >
+            {currencies.map((data) => (
+              <option key={ data } value={ data }>{data}</option>
+            ))}
+          </select>
+        </div>
+        <div className="currency-button-container">
+          <button className="currency-button" type="submit">
+            {editor ? 'Editar despesa' : 'Adicionar despesa'}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
